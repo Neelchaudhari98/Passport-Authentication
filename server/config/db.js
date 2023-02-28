0b2d49c71ede22819cab45bdc439f7228b94be0c
Neelchaudhari98 <neelchaudhari98@gmail.com>
2023-02-28 10:54:26 -0500

    First Commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = 
+{
+   // "URI": "mongodb://localhost/contact_list"
+    "URI": "mongodb+srv://neelchaudhari98:mypassword@cluster0.v5x8yme.mongodb.net/?retryWrites=true&w=majority"
+}       //added database URI
