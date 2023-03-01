d95f4ad3cc2c6875038c37a83ac5f146dcfe270f
Neelchaudhari98 <neelchaudhari98@gmail.com>
2023-03-01 11:09:32 -0500

    second

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = 
 {
    // "URI": "mongodb://localhost/contact_list"
-    "URI": "mongodb+srv://neelchaudhari98:mypassword@cluster0.v5x8yme.mongodb.net/?retryWrites=true&w=majority"
+    "URI": "mongodb+srv://doadmin:ACYH7PD6529V8S30@db-mongodb-tor1-51839-255657a7.mongo.ondigitalocean.com/admin?replicaSet=db-mongodb-tor1-51839&tls=true&authSource=admin"
 }       //added database URI
